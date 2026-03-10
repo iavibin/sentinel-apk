ea1b540d8f57dd14bcb0858b923dc8413f7e5e00
yajasnitin822 <yajasnitin822@gmail.com>
2026-03-10 15:41:23 +0530

    fix: update service name and register ResultActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,4 +88,3 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-}
